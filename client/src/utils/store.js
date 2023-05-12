a2b29de4c1146b89b4058f71b1e301d9b48304a1
ikaera <ikaera@gmail.com>
2023-05-12 12:55:14 -0400

    add store, import Privider to the index.js.

--- client/src/utils/store.js
+++ client/src/utils/store.js
@@ -1,6 +1,54 @@
 import { legacy_createStore } from 'redux';
 import reducer from './reducers';
 
-const store = legacy_createStore(reducer);
+const counterReducer = (state = { counter: 0 }, action) => {
+  if (action.type === 'increment') {
+    return {
+      counter: state.counter + 1,
+    };
+  }
+
+  if (action.type === 'decrement') {
+    return {
+      counter: state.counter - 1,
+    };
+  }
+  return state;
+};
+
+const store = legacy_createStore(counterReducer);
 
 export default store;
+
+/*
+const redux = require('redux');
+
+const counterReducer = (state = { counter: 0 }, action) => {
+  if (action.type === 'increment') {
+    return {
+      counter: state.counter + 1,
+    };
+  }
+
+  if (action.type === 'decrement') {
+    return {
+      counter: state.counter - 1,
+    };
+  }
+};
+
+const store = redux.createStore(counterReducer);
+
+console.log(store.getState());
+
+const counterSubscriber = () => {
+  const latestState = store.getState();
+  console.log(latestState);
+};
+
+store.subscribe(counterSubscriber);
+
+store.dispatch({ type: 'increment' });
+store.dispatch({ type: 'decrement' });
+
+*/
